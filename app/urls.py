5c4f075ff1bb0f2a1238d10c43dc012c1f2a2a0a
lenfantterrible <mehdiat6@gmail.com>
2021-06-03 17:51:15 +0430

    Add Ackerman Function

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from .views import fibonacci
+from .views import fibonacci, ackerman
 
 app_name = 'app'
 
 urlpatterns = [
 
     path('fibonacci/', fibonacci, name='fibonacci'),
+    path('ackerman/', ackerman, name='ackerman'),
 
 ]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 def calculate_fib(number):
 
     if number in [1,2]:
@@ -27,3 +26,31 @@ def fibonacci(request):
         return render(request, 'fib.html', {'answer': calculate_fib(inp)})
     else:
         return render(request, 'fib.html', {})
+
+def calculate_ackerman(m, n):
+    if m == 0:
+        return n + 1
+
+    if n == 0:
+        return calculate_ackerman(m - 1, 1)
+    
+    n2 = calculate_ackerman(m, n - 1)
+
+    return calculate_ackerman(m - 1, n2)
+
+
+def ackerman(request):
+    if request.method == 'POST':
+        try:
+            m = int(request.POST['m']) 
+            n = int(request.POST['n']) 
+        except:
+            return render(request, 'ack.html', {'error': 'Please Enter a Valid Number'})
+        
+        if m <= 0 or n <= 0:
+            return render(request, 'ack.html', {'error': 'Please Enter a Positive Integer'})
+
+
+        return render(request, 'ack.html', {'answer': calculate_ackerman(m, n)})
+    else:
+        return render(request, 'ack.html', {})
