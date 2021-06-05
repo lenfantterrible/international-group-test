4280f9fa0c5c864e5f7cbceb5b906bcc1d615614
lenfantterrible <mehdiat6@gmail.com>
2021-06-05 19:22:25 +0430

    Use Class-Based Views

--- app/urls.py
+++ app/urls.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from .views import fibonacci, ackerman, factorial, HomeView
+from .views import FiboView, AckermanView, FactView, HomeView
 
 app_name = 'app'
 
 urlpatterns = [
 
-    path('fibonacci/', fibonacci, name='fibonacci'),
-    path('ackerman/', ackerman, name='ackerman'),
-    path('factorial/', factorial, name='factorial'),
+    path('fibonacci/', FiboView.as_view(), name='fibonacci'),
+    path('ackerman/', AckermanView.as_view(), name='ackerman'),
+    path('factorial/', FactView.as_view(), name='factorial'),
     path('', HomeView.as_view(), name='home'),
 
 
--- app/views.py
+++ app/views.py
@@ -4,6 +4,7 @@ from django.views.generic import TemplateView
 from .forms import AckermanForm, FactForm, FiboForm 
 from django.views import View
 
+
 class HomeView(TemplateView):
     template_name = "base.html"
 
@@ -11,15 +12,30 @@ class FiboView(View):
     form_class = FiboForm
     template_name = 'fib.html'
 
+    @staticmethod
+    def calculate_fib(number):
+
+        if number in [1,2]:
+            return 1 
+        
+        first = 1 
+        second = 1 
+        for i in range(2,number):
+            second += first 
+            first = second - first 
+
+        return second
+
+
     def get(self, request, *args, **kwargs):
-        form = self.form_class(initial=self.initial)
+        form = self.form_class()
         return render(request, self.template_name, {'form': form})
 
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         if form.is_valid():
             time1 = perf_counter()
-            answer = calculate_fib(form.cleaned_data['n'])
+            answer = FiboView.calculate_fib(form.cleaned_data['n'])
             time2 = perf_counter()
 
             return render(request, self.template_name, {'answer': f'{answer}\n calculated in {(time2 - time1)*1000:0.10f} milliseconds', 'form': form})
@@ -27,155 +43,78 @@ class FiboView(View):
             return render(request, self.template_name, {'form': form})  
 
 
+class AckermanView(View):
+    form_class = AckermanForm
+    template_name = 'ack.html'
 
-
-def calculate_fib(number):
-
-    if number in [1,2]:
-        return 1 
-    
-    first = 1 
-    second = 1 
-    for i in range(2,number):
-        second += first 
-        first = second - first 
-
-    return second
-
-# def fibonacci(request):
-#     if request.method == 'POST':
-#         try:
-#             inp = int(request.POST['inp']) 
-#         except:
-#             return render(request, 'fib.html', {'error': 'Please Enter a Valid Number'})
-        
-#         if inp <= 0:
-#             return render(request, 'fib.html', {'error': 'Please Enter a Positive Integer'})
-
-#         time1 = perf_counter()
-#         answer = calculate_fib(inp)
-#         time2 = perf_counter()
-
-#         return render(request, 'fib.html', {'answer': f'{answer}\n calculated in {(time2 - time1)*1000:0.10f} milliseconds' })
-#     else:
-#         return render(request, 'fib.html', {})
-
-def fibonacci(request):
-    if request.method == 'POST':
-        form = FiboForm(request.POST)
-        
-        if form.is_valid():
-            time1 = perf_counter()
-            answer = calculate_fib(form.cleaned_data['n'])
-            time2 = perf_counter()
-
-            return render(request, 'fib.html', {'answer': f'{answer}\n calculated in {(time2 - time1)*1000:0.10f} milliseconds', 'form': form})
-        else: 
-            return render(request, 'fib.html', {'form': form})  
-    else:
-        form = FiboForm()
-        return render(request,'fib.html', {'form': form})
-
-
-def calculate_ackerman(m, n):
+    @staticmethod
+    def calculate_ackerman(m, n):
   
-    # creating 2D LIST
-    cache = [[0 for i in range(n + 1)] for j in range(m + 1)]
-    for rows in range(m + 1):
-        for cols in range(n + 1):
-            # base case A ( 0, n ) = n + 1
-            if rows == 0:       
-                cache[rows][cols] = cols + 1
-            # base case  A ( m, 0 ) = 
-            # A ( m-1, 1) [Computed already]
-            elif cols == 0:
-                cache[rows][cols] = cache[rows-1][1]
-            else:
-                # if rows and cols > 0
-                # then applying A ( m, n ) = 
-                # A ( m-1, A ( m, n-1 ) ) 
-                r = rows - 1
-                c = cache[rows][cols-1]
-                # applying equation (2) 
-                # here A ( 0, n ) = n + 1
-                if r == 0:    
-                    ans = c + 1
-                elif c <= n:
-                    # using stored value in cache
-                    ans = cache[rows-1][cache[rows][cols-1]]
+        cache = [[0 for i in range(n + 1)] for j in range(m + 1)]
+        for rows in range(m + 1):
+            for cols in range(n + 1):
+                if rows == 0:       
+                    cache[rows][cols] = cols + 1
+
+                elif cols == 0:
+                    cache[rows][cols] = cache[rows-1][1]
                 else:
-                    # Using the Derived Formula 
-                    # to compute mystery values in O(1) time
-                    ans = (c-n)*(r) + cache[r][n]
-  
-                cache[rows][cols] = ans
-  
-    return cache[m][n]
 
+                    r = rows - 1
+                    c = cache[rows][cols-1]
 
-# def ackerman(request):
-#     if request.method == 'POST':
-#         try:
-#             m = int(request.POST['m']) 
-#             n = int(request.POST['n']) 
-#         except:
-#             return render(request, 'ack.html', {'error': 'Please Enter a Valid Number'})
-        
-#         if m <= 0 or n <= 0:
-#             return render(request, 'ack.html', {'error': 'Please Enter a Positive Integer'})
-
-#         time1 = perf_counter()
-#         answer = calculate_ackerman(m, n)
-#         time2 = perf_counter()
-
-#         return render(request, 'ack.html', {'answer': f'{answer}\n calculated in {(time2 - time1)*1000:0.10f} milliseconds' })
-
-#     else:
-#         return render(request, 'ack.html', {})
-
-def ackerman(request):
-    if request.method == 'POST':
-
-        form = AckermanForm(request.POST)
-        
-        if form.is_valid():
-            time1 = perf_counter()
-            answer = calculate_ackerman(form.cleaned_data['m'], form.cleaned_data['n'])
-            time2 = perf_counter()
-
-            return render(request, 'ack.html', {'answer': f'{answer}\n calculated in {(time2 - time1)*1000:0.10f} milliseconds', 'form': form})
-
-        else: 
-            return render(request, 'ack.html', {'form': form})
-    else:
-        form = AckermanForm()
-        return render(request, 'ack.html', {'form': form})
-
-
-
-def fact(n):
-    out = 1 
-    for i in range(1,n+1):
-        out *= i
-
-    return out
-
-
-def factorial(request):
-    if request.method == 'POST':
-        
-        form = FactForm(request.POST)
-        
-        if form.is_valid():
-            time1 = perf_counter()
-            answer = fact(form.cleaned_data['n'])
-            time2 = perf_counter()
+                    if r == 0:    
+                        ans = c + 1
+                    elif c <= n:
+                        ans = cache[rows-1][cache[rows][cols-1]]
+                    else:
+                        ans = (c-n)*(r) + cache[r][n]
     
-            return render(request, 'fact.html', {'answer': f'{answer},   calculated in {(time2 - time1)*1000:0.10f} milliseconds', 'form': form })
+                    cache[rows][cols] = ans
+    
+        return cache[m][n]
 
-        else:
-            return render(request, 'fact.html', {'form': form})
 
-    else:
-        form = FactForm()
-        return render(request, 'fact.html', {'form': form})
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            time1 = perf_counter()
+            answer = AckermanView.calculate_ackerman(form.cleaned_data['m'], form.cleaned_data['n'])
+            time2 = perf_counter()
+
+            return render(request, self.template_name, {'answer': f'{answer}\n calculated in {(time2 - time1)*1000:0.10f} milliseconds', 'form': form})
+        else: 
+            return render(request, self.template_name, {'form': form})  
+
+
+class FactView(View):
+    form_class = FactForm
+    template_name = 'fact.html'
+
+    @staticmethod
+    def fact(n):
+        out = 1 
+        for i in range(1,n+1):
+            out *= i
+
+        return out
+
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            time1 = perf_counter()
+            answer = FactView.fact(form.cleaned_data['n'])
+            time2 = perf_counter()
+
+            return render(request, self.template_name, {'answer': f'{answer}\n calculated in {(time2 - time1)*1000:0.10f} milliseconds', 'form': form})
+        else: 
+            return render(request, self.template_name, {'form': form})  
